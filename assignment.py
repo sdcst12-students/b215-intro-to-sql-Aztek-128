e06a38f78612e90cffcec6b8c34621d6d89867f2
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-10-31 13:16:09 -0700

    yes

--- assignment.py
+++ assignment.py
@@ -27,10 +27,10 @@ class main():
     def __init__(self) -> None:
         
         file = 'dbase.db'
-        connection = sqlite3.connect(file)
-        cursor = connection.cursor()
+        self.connection = sqlite3.connect(file)
+        self.cursor = self.connection.cursor()
         query = """
-        create table if not exists customers (
+        create table if not exists petstore (
             id integer primary key autoincrement,
             petname tinytext,
             species tinytext,
@@ -41,11 +41,11 @@ class main():
             balance int,
             date tinytext);
         """
-        cursor.execute(query)
+        self.cursor.execute(query)
         self.decision()
 
     def decision(self):
-        choice = int(input("would you like to do: 1)insert new record, 2)retrieve record by id, 3)retrieve record by email, 4) retrieve record by phone number"))
+        choice = int(input("would you like to do: 1)insert new record, 2)retrieve record by id, 3)retrieve record by email, 4) retrieve record by phone number: "))
         if choice == 1:
             self.insert()
         elif choice == 2:
@@ -54,6 +54,8 @@ class main():
             self.RetrieveEmail()
         elif choice == 4:
             self.RetrievePH()
+        elif choice == 5:
+            self.getall()
 
     def insert(self):
         petname = input("enter pets name:   ")
@@ -64,6 +66,32 @@ class main():
         email = input("enter email:     ")
         balance = int(input("enter what owner owes:     "))
         date = input("enter the date of first visit:    ")
-        query = f"insert into customers (petname,species,breed,name,phone,email,balance,date) values ('{petname}','{species}','{breed}','{name}','{phone_number}','{}','{}','{}','{}',)"
+        query = f"insert into petstore (petname,species,breed,name,phone,email,balance,date) values ('{petname}','{species}','{breed}','{name}','{phone_number}','{email}','{balance}','{date}');"
+        self.cursor.execute(query)
+        self.connection.commit()
+    def RetrieveID(self):
+        ID = int(input("enter in the ID of the person you are searching:    "))
+        query = f"select * from petstore where id=={ID}"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(result)
+        
+    def RetrieveEmail(self):
+        email = str(input("enter in the email of the person you are searching:  "))
+        query = f"select * from petstore where email=='{email}'"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()#Too scary cant look. Please help me it is ansons fac
+        print(result)
+    def RetrievePH(self):
+        PH = int(input("enter in the phone number of the person you are searching:    "))
+        query = f"select * from petstore where phone=={PH}"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(result)
+    def getall(self):
+        query = "select * from petstore"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(result)
 
 main()
